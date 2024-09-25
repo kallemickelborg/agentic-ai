68a7e87a4156baba7ab786f7d49454bc6fbae69a
Mick Kalle Mickelborg <mkm@dynacap.dk>
2024-09-26 00:26:31 +0200

    Redirect fix for Netlify

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 
-          process.env.NODE_ENV === 'development'
-            ? 'http://localhost:5000/api/:path*'
-            : '/api/:path*',
-      },
-    ]
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
 };
 
