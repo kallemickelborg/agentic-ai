d4222a9a8c6fe1e84e7944a9e359935eb52380ce
Mick Kalle Mickelborg <mkm@dynacap.dk>
2024-09-26 16:51:07 +0200

    Added backend directory

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,10 +1,11 @@
 import sys
 import os
 from mangum import Mangum
-from netlify.functions.backend.app import app  # Import your main FastAPI app
 
 # Add the backend directory to the Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'backend'))
+
+from backend.app import app  # Import your main FastAPI app
 
 # Create a handler for the FastAPI app
 handler = Mangum(app)
