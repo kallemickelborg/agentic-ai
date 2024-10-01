eed68307daebc11a4a9a189774aa7bc5eb5d99b9
Mick Kalle Mickelborg <kallemickelborg@gmail.com>
2024-10-01 13:13:04 +0200

    Update Render frontend / backend config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: 'https://agentic-ai-backend.onrender.com',
+  },
 //   assetPrefix: './',
 };
 
